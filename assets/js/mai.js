1e828ad3fa5d2798179c88b3e9c4ae973933e04b
ErasmoPalacios <erpp2001@gmail.com>
2021-03-14 10:50:22 -0600

    pagina web pro

---
+++ assets/js/mai.js
@@ -0,0 +1,16 @@
+let menuClick = document.getElementById("menu-click")
+let enlaces = document.querySelector(".enlaces-header")
+
+
+//menuClick.addEventListener("click", funtion(){
+
+//});
+
+
+menuClick.addEventListener("click",()=> {
+    enlaces.classList.toggle("click");
+})
+
+
+
+console.log(enlaces);
